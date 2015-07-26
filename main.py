10f486c75a384b50a445cad28eb8aa34df38b990
Eric Hanchrow <eric.hanchrow@gmail.com>
2015-07-26 10:42:52 -0700

    Huh, this actually works.
    As expected, running it twice in a row gets you two mostly-identical
    cards, so I guess I gotta search by title, or something, to make it
    idempotent.

--- main.py
+++ main.py
@@ -8,4 +8,22 @@ client = TrelloClient(
     **py_trello_auth_dict
 )
 
-print(client.list_boards())
+board = client.get_board ('hPK2DcxK')
+print("About to munge {!r}".format(board.name))
+
+input_data = [{'name': 'Some Test Book', 'desc': '''
+This is _such_ a great book!  Here's a quote from it:
+> Twas brillig, yo
+> The slithey toves
+> Were all like up in your shit
+'''}]
+
+lists = board.get_lists(None)
+if len(lists) != 1:
+    raise Exception("{!r} has {} entries, but I only know how to deal with a single entry".format(lists, len(lists)) )
+
+l = lists[0]
+print("List: {!r}".format(l))
+
+for book in input_data:
+    l.add_card(**book)
